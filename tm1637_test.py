b79d58053cb9cb4d1edef436ce67864e26313d34
coliss86 <coliss86@yahoo.fr>
2019-01-27 18:23:01 +0100

    Add pause beetween test to record it easily

--- tm1637_test.py
+++ tm1637_test.py
@@ -2,10 +2,13 @@
 
 # Raspberry Pi Python 3 TM1637 quad 7-segment LED display driver examples
 
-import tm1637
+import sys
 
-CLK=1
-DIO=0
+import tm1637
+from time import sleep
+
+CLK=7
+DIO=8
 
 tm = tm1637.TM1637(clk=CLK, dio=DIO)
 
@@ -15,23 +18,32 @@ tm.write(bytearray([127, 255, 127, 127]))
 tm.write(b'\x7F\xFF\x7F\x7F')
 tm.show('8888', True)
 tm.numbers(88, 88, True)
+sleep(0.5)
+
+for i in range(7):
+    tm.brightness(i)
+    sleep(0.1)
 
 # all LEDS off
 tm.write([0, 0, 0, 0])
 tm.show('    ')
+sleep(0.5)
 
 # write to the 2nd and 3rd segments only
 tm.write([119, 124], 1) # _Ab_
 tm.write([124], 2) # __b_
 tm.write([119], 1) # _A__
+sleep(0.5)
 
 # display "0123"
 tm.write([63, 6, 91, 79])
 tm.write(bytearray([63, 6, 91, 79]))
 tm.write(b'\x3F\x06\x5B\x4F')
+sleep(0.5)
 tm.show('1234')
 tm.number(1234)
 tm.numbers(12, 34, False)
+sleep(0.5)
 
 # display "4567"
 tm.write([102, 109, 125, 7])
@@ -39,23 +51,26 @@ tm.write([102], 0) # 4___
 tm.write([109], 1) # _5__
 tm.write([125], 2) # __6_
 tm.write([7], 3)   # ___7
+sleep(0.5)
 
 # set middle two segments to "12", ie "4127"
 tm.write([6, 91], 1) # _12_
+sleep(0.5)
 
 # set last segment to "9", ie "4129"
 tm.write([111], 3) # ___9
+sleep(0.5)
 
 # walk through all possible LED combinations
-from time import sleep_ms
-for i in range(128):
-    tm.number(i)
-    tm.write([i])
-    sleep_ms(100)
+#for i in range(128):
+#    tm.number(i)
+#    tm.write([i])
+#    sleep(0.1)
 
 # show "AbCd"
 tm.write([119, 124, 57, 94])
 tm.show('abcd')
+sleep(0.5)
 
 # show "COOL"
 tm.write([0b00111001, 0b00111111, 0b00111111, 0b00111000])
@@ -64,29 +79,35 @@ tm.write(b'\x39\x3F\x3F\x38')
 tm.write([57, 63, 63, 56])
 tm.show('cool')
 tm.show('COOL')
+sleep(0.5)
 
 # display "dEAd", "bEEF"
 tm.hex(0xdead)
 tm.hex(0xbeef)
 tm.show('dead')
+sleep(0.5)
 tm.show('Beef')
+sleep(0.5)
 
 # show "12:59"
 tm.numbers(12, 59)
 tm.show('1259', True)
+sleep(0.5)
 
 # show "-123"
 tm.number(-123)
 tm.show('-123')
+sleep(0.5)
 
 # Show Help
-tm.show('Help')
-tm.write(tm.encode_string('Help'))
-tm.write([tm.encode_char('H'), tm.encode_char('e'), tm.encode_char('l'), tm.encode_char('p')])
+#tm.show('Help')
+#tm.write(tm.encode_string('Help'))
+#tm.write([tm.encode_char('H'), tm.encode_char('e'), tm.encode_char('l'), tm.encode_char('p')])
+#sleep(0.5)
 
 # Scroll Hello World from right to left
 tm.scroll('Hello World') # 4 fps
-tm.scroll('Hello World', 1000) # 1 fps
+#tm.scroll('Hello World', 1000) # 1 fps
 
 # Scroll all available characters
 tm.scroll(list(tm1637._SEGMENTS))
@@ -157,57 +178,82 @@ tm.encode_char('F')
 # 113
 
 # display "dEAd", "bEEF", "CAFE" and "bAbE"
-tm.hex(0xdead)
-tm.hex(0xbeef)
-tm.hex(0xcafe)
-tm.hex(0xbabe)
+#tm.hex(0xdead)
+#sleep(0.5)
+#tm.hex(0xbeef)
+#sleep(0.5)
+#tm.hex(0xcafe)
+#sleep(0.5)
+#tm.hex(0xbabe)
+#sleep(0.5)
 
 # show "00FF" (hex right aligned)
-tm.hex(0xff)
+#tm.hex(0xff)
+#sleep(0.5)
 
 # show "   1" (numbers right aligned)
-tm.number(1)
+#tm.number(1)
+#sleep(0.5)
 
 # show "  12"
-tm.number(12)
+#tm.number(12)
+#sleep(0.5)
 
 # show " 123"
-tm.number(123)
+#tm.number(123)
+#sleep(0.5)
 
 # show "9999" capped at 9999
-tm.number(20000)
+#tm.number(20000)
+#sleep(0.5)
 
 # show "  -1"
-tm.number(-1)
+#tm.number(-1)
+#sleep(0.5)
 
 # show " -12"
-tm.number(-12)
+#tm.number(-12)
+#sleep(0.5)
 
 # show "-123"
 tm.number(-123)
+sleep(0.5)
 
 # show "-999" capped at -999
 tm.number(-1234)
+sleep(0.5)
 
 # show "01:02"
 tm.numbers(1, 2)
+sleep(0.5)
 
 # show "0102"
 tm.numbers(1, 2, False)
+sleep(0.5)
 
 # show "-5:11"
 tm.numbers(-5, 11)
+sleep(0.5)
 
 # show "12:59"
 tm.numbers(12, 59)
+sleep(0.5)
 
 # show temperature '24*C'
 tm.temperature(24)
 tm.show('24*C')
+sleep(0.5)
 
 # show temperature works for range -9 to +99
 tm.temperature(-10) # LO*C
+sleep(0.5)
 tm.temperature(-9)  # -9*C
+sleep(0.5)
 tm.temperature(5)   #  5*C
+sleep(0.5)
 tm.temperature(99)  # 99*C
+sleep(0.5)
 tm.temperature(100) # HI*C
+sleep(0.5)
+
+tm.write([0, 0, 0, 0])
